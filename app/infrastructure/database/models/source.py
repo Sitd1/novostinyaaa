3d14428eab9edc48fd3bf6efbbc1dbac26233b7e
denis.sitdikov <denis.sitd@yandex.ru>
2025-12-01 13:17:30 +0300

    ref models Source, RawNewsORM

---
+++ app/infrastructure/database/models/source.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from database.models.base import Base
+
+
+class SourceORM(Base):
+    """Таблица справочник для хранения источников новостей, значения должны быть уникальными"""
+    __tablename__ = "source"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    type: Mapped[str] = mapped_column(String(255), index=True)
+    name: Mapped[str] = mapped_column(String(32), index=True)
+    title: Mapped[str] = mapped_column(String(255), index=True)
+    external_id: Mapped[str | None] = mapped_column(String(50), index=True)
+    description: Mapped[str | None] = mapped_column(String(255), index=True)
+    url: Mapped[str | None] = mapped_column(String(255), index=True)
+
+    # связь 1 → N
+    raw_news: Mapped[list["RawNewsORM"]] = relationship(
+        back_populates="source", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self) -> str:
+        return f"<src_name={self.name} src_title={self.title}> description={self.description}>"
