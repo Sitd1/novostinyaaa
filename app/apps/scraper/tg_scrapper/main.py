56d637fac8fafe97a38e4e239dd7225337fe6774
denis.sitdikov <denis.sitd@yandex.ru>
2025-12-01 09:22:04 +0300

    add dummies

---
+++ app/apps/scraper/tg_scrapper/main.py
@@ -0,0 +1,3 @@
+# Скрипт, который раз в N минут тянет новости из TG и кладёт в БД →
+# app/apps/scraper/tg_scrapper/main.py
+# (внутри: создаём реализации портов из infra, вызываем use-case ingest_raw_news).
