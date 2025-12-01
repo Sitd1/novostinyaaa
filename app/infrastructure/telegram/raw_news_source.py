c0c14b117dd75e65f1b604db463d34fac36baa32
denis.sitdikov <denis.sitd@yandex.ru>
2025-12-01 09:34:07 +0300

    add dummies

--- app/infrastructure/telegram/raw_news_source.py
+++ app/infrastructure/telegram/raw_news_source.py
@@ -2,3 +2,6 @@
 # ExternalRawNewsSource и DTO ExternalRawNewsItem у тебя в
 # app/core/application/raw_news/dto/external_raw_news_item.py и
 # .../ports/ingest.py.
+
+# async def fetch_new_raw_items(self) -> Iterable[ExternalRawNewsItem]:
+#     # использует client.fetch_channel_messages(...)
