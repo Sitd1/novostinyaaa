9fa3d8f263860b9f4dccd06db41de8033b950d64
denis.sitdikov <denis.sitd@yandex.ru>
2025-12-01 12:54:27 +0300

    add models Source, RawNewsORM

--- app/infrastructure/database/models/raw_news.py
+++ app/infrastructure/database/models/raw_news.py
@@ -1,13 +1,23 @@
+from decimal import Decimal
+
 from database.models.base import Base
 
-
-# models.py
 from datetime import datetime
 from typing import Optional
 
 from sqlalchemy import DateTime, Integer, String, Text, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
+
+class Source(Base):
+    __tablename__ = "source"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    type: Mapped[str] = mapped_column(String(255), index=True)
+    name: Mapped[str] = mapped_column(String(32), index=True)
+    title: Mapped[str] = mapped_column(String(255), index=True)
+    external_id: Mapped[str | None] = mapped_column(String(50), index=True)
+    description: Mapped[str | None] = mapped_column(String(255), index=True)
+    url: Mapped[str | None] = mapped_column(String(255), index=True)
 
 
 class RawNewsORM(Base):
@@ -16,18 +26,29 @@ class RawNewsORM(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
-    # Идентификатор канала
-    # channel_id: Mapped[int] = mapped_column(BigInteger, index=True)
-    channel_username: Mapped[Optional[str]] = mapped_column(String(255), index=True, nullable=True)
+    # Идентификатор сообщения внутри канала / source
+    external_id: Mapped[int] = mapped_column(Integer, index=True)
 
-    # Идентификатор сообщения внутри канала
-    message_id: Mapped[int] = mapped_column(Integer, index=True)
+    # Дата и время публикации
+    published_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True)
 
-    # Дата сообщения
-    date: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True)
+    # Дата получения, по умолчанию now()
+    fetched_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True)
 
     # Текст сообщения (без медиа)
-    text: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+    text: Mapped[Optional[str]] = mapped_column(nullable=True)
+
+    # Ссылка на саму новость
+    url: Mapped[Optional[str]] = mapped_column(nullable=True)
+
+    # Значения, которые будут считаться и обновляться
+
+    # tags (скорее набор из нескольких значений)
+    tags: Mapped[str] = mapped_column(nullable=True)
+    importance: Mapped[Decimal] = mapped_column(nullable=True)
+    summary: Mapped[str] = mapped_column(nullable=True)
+
+    source_id: Mapped[str]  # ToDo foreign key
 
     # Можно добавить ещё поля (link, views, etc.)
 
@@ -36,4 +57,4 @@ class RawNewsORM(Base):
     )
 
     def __repr__(self) -> str:
-        return f"<TgMessage channel={self.channel_username} msg_id={self.message_id}>"
+        return f"<TgMessage channel={self.channel_username} msg_id={self.message_id}> pulished_at={self.published_at}>"
