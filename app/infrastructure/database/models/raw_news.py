8228fa61fe8d33c0031c467ff6fdd5bc960d34f9
denis.sitdikov <denis.sitd@yandex.ru>
2025-12-01 13:13:20 +0300

    add models Source, RawNewsORM

--- app/infrastructure/database/models/raw_news.py
+++ app/infrastructure/database/models/raw_news.py
@@ -1,15 +1,15 @@
 from decimal import Decimal
 
+from datetime import datetime
+
+from sqlalchemy import DateTime, Integer, String, Text, UniqueConstraint, ForeignKey, Numeric
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
 from database.models.base import Base
 
-from datetime import datetime
-from typing import Optional
 
-from sqlalchemy import DateTime, Integer, String, Text, UniqueConstraint
-from sqlalchemy.orm import Mapped, mapped_column
-
-
-class Source(Base):
+class SourceORM(Base):
+    """Таблица справочник для хранения источников новостей, значения должны быть уникальными"""
     __tablename__ = "source"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     type: Mapped[str] = mapped_column(String(255), index=True)
@@ -19,9 +19,17 @@ class Source(Base):
     description: Mapped[str | None] = mapped_column(String(255), index=True)
     url: Mapped[str | None] = mapped_column(String(255), index=True)
 
+    # связь 1 → N
+    raw_news: Mapped[list["RawNewsORM"]] = relationship(
+        back_populates="source", cascade="all, delete-orphan"
+    )
+
+    def __repr__(self) -> str:
+        return f"<src_name={self.name} src_title={self.title}> description={self.description}>"
+
+
 
 class RawNewsORM(Base):
-    # ToDo - переписать как в domain
     __tablename__ = "tg_raw_news"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
@@ -36,25 +44,27 @@ class RawNewsORM(Base):
     fetched_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), index=True)
 
     # Текст сообщения (без медиа)
-    text: Mapped[Optional[str]] = mapped_column(nullable=True)
+    text: Mapped[str | None] = mapped_column(nullable=True)
 
     # Ссылка на саму новость
-    url: Mapped[Optional[str]] = mapped_column(nullable=True)
+    url: Mapped[str | None] = mapped_column(String(255), nullable=True)
 
     # Значения, которые будут считаться и обновляться
 
-    # tags (скорее набор из нескольких значений)
-    tags: Mapped[str] = mapped_column(nullable=True)
-    importance: Mapped[Decimal] = mapped_column(nullable=True)
-    summary: Mapped[str] = mapped_column(nullable=True)
+    # Вариант: tags храним JSON (лучше всего) - должно быть несколько значений
+    tags: Mapped[str | None] = mapped_column(Text, nullable=True)
+    importance: Mapped[Decimal] = mapped_column(Numeric(5, 2), nullable=True)
+    summary: Mapped[str] = mapped_column(Text, nullable=True)
 
-    source_id: Mapped[str]  # ToDo foreign key
+    # foreign key
+    source_fk: Mapped[int] = mapped_column(ForeignKey("source.id"), index=True)
 
-    # Можно добавить ещё поля (link, views, etc.)
+    # ORM связь
+    source: Mapped[SourceORM] = relationship(back_populates="raw_news")
 
     __table_args__ = (
-        UniqueConstraint( "channel_username", "message_id", name="uq_channel_message"),
+        UniqueConstraint("source_fk", "external_id", name="uq_source_msg"),
     )
 
     def __repr__(self) -> str:
-        return f"<TgMessage channel={self.channel_username} msg_id={self.message_id}> pulished_at={self.published_at}>"
+        return f"<RawNews id={self.id} src={self.source_fk} ext_id={self.external_id}>"
