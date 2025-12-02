197ee4185dfb7b238a66fbc495bcfb7275570243
denis.sitdikov <denis.sitd@yandex.ru>
2025-12-02 09:40:41 +0300

    ref mappers

--- app/infrastructure/database/models/__init__.py
+++ app/infrastructure/database/models/__init__.py
@@ -1,6 +1,13 @@
 from .base import Base
 from .news import NewsEventORM
 from .raw_news import RawNewsORM
-from .runs_story import RunsStory
+from .news_event_raw_news_mapper import NewsEventRawNewsORM
+from .source import NewsSourceORM
+from .news_event_tags_mapper import NewsEventTagORM
 
-__all__ = ["Base", "RawNewsORM", "NewsEventORM", "RunsStory"]
+
+__all__ = [
+    "Base", "RawNewsORM", "NewsEventORM",
+    "NewsEventRawNewsORM", "NewsSourceORM",
+    "NewsEventTagORM"
+]
