b069ff4b1c51ae0c114dfdd1cbedbf3cf2389ee3
denis.sitdikov <denis.sitd@yandex.ru>
2025-12-02 09:52:59 +0300

    add migrations

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.config.settings import config as config_settings
-from app.infrastructure.database.models.base import Base
+from app.infrastructure.database.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
