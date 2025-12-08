a86c43e00ffbbea9840702b89d7107ab3df13f45
denis.sitdikov <denis.sitd@yandex.ru>
2025-12-08 17:17:18 +0300

    upd similarity use case

--- app/core/application/raw_news/use_cases/similarity.py
+++ app/core/application/raw_news/use_cases/similarity.py
@@ -5,6 +5,7 @@
 # Работа с RawNews закончена
 from __future__ import annotations
 
+import asyncio
 from datetime import timedelta
 
 from app.core.domain.raw_news.entities import RawNews
@@ -20,12 +21,12 @@ from models import raw_news
 
 class RawNewsSimilarityUseCase:
     def __init__(
-            self,
-            raw_repo: RawNewsRepository,
-            events_repo: NewsEventRepository,
-            similarity_matcher: SimilarityMatcherService,
-            config: ClusteringConfig | None = None,
-            limit: int | None = None,
+        self,
+        raw_repo: RawNewsRepository,
+        events_repo: NewsEventRepository,
+        similarity_matcher: SimilarityMatcherService,
+        config: ClusteringConfig | None = None,
+        limit: int | None = None,
     ):
         self.raw_repo = raw_repo
         self.events_repo = events_repo
@@ -39,26 +40,30 @@ class RawNewsSimilarityUseCase:
         self.new_events: list[NewsEvent] = []
         self.updated_events: list[NewsEvent] = []
 
-    async def process_one_raw_news(self, raw: RawNews) -> RawNews:
+    async def process_one_raw_news(self, raw: RawNews) -> None:
         """Ищем кандидаты-события - получить небольшой список событий,
-        с которыми можно сравнить текущую новость.
-        Смотрим ближайшие и косинусное расстояние отсеиваем по threshold также смотрим прочие фильтры
-        """
+                с которыми можно сравнить текущую новость.
+                Смотрим ближайшие и косинусное расстояние отсеиваем по threshold также смотрим прочие фильтры
+                """
 
         # совпадение по диапазону времени window_start, window_end и прочее по конфигу
-        event_candidates: list[NewsEvent] = self.events_repo.get_news_events_candidates(raw, self.config)
+        event_candidates = self.events_repo.get_news_events_candidates(raw, self.config)
 
         if event_candidates:
-            similarity_candidates: dict[int, int] = await self.similarity_matcher.get_similar_news_candidates(raw, self.config)
+            similarity_candidates = await self.similarity_matcher.get_similar_news_candidates(
+                raw, self.config
+            )
         else:
-            similarity_candidates: dict[int, int] = dict()
+            similarity_candidates = {}
 
         # Создание нового события (нет подходящих events)
-        if len(similarity_candidates.keys()) == 0:
-            event = await self.events_repo.create_from_raw_news(raw)  # новое событие
+        if not similarity_candidates:
+            event = await self.events_repo.create_from_raw_news(raw)
             self.new_events.append(event)
         else:
-            chosen_event = await self.similarity_matcher.similaritychoose_event_for_raw_news(raw, similarity_candidates)
+            chosen_event = await self.similarity_matcher.similaritychoose_event_for_raw_news(
+                raw, similarity_candidates
+            )
             if chosen_event is None:
                 event = await self.events_repo.create_from_raw_news(raw)
                 self.new_events.append(event)
@@ -71,96 +76,17 @@ class RawNewsSimilarityUseCase:
         self.updated_raw_news.append(raw)
 
     async def execute_raw_news(self) -> list[RawNews]:
-        self.executed_raw_news: list[RawNews] = await self.raw_repo.list_pending_for_event_clustering(limit=self.limit)
+        self.executed_raw_news = await self.raw_repo.list_pending_for_event_clustering(
+            limit=self.limit
+        )
         return self.executed_raw_news
 
     async def process_raw_news(self):
-        for raw_news in await self.execute_raw_news():
-            await self.process_one_raw_news(raw_news)
+        # можно распараллелить (только если репозитории поддерживают многопоточность
+        raw_list = await self.execute_raw_news()
+        await asyncio.gather(*(self.process_one_raw_news(raw) for raw in raw_list))
 
     async def save_updates(self) -> None:
-        await self.events_repo.save_many(self.new_events)
+        await self.raw_repo.update_many(self.updated_raw_news)
         await self.events_repo.update_many(self.updated_events)
-        await self.raw_repo.update_many(self.updated_raw_news)  # обновляем raw_repo
-
-
-
-async def cluster_pending_raw_news(
-        raw_repo: RawNewsRepository,
-        events_repo: NewsEventRepository,
-        similarity_matcher: SimilarityMatcherService,
-        config: ClusteringConfig | None = None,
-        limit: int | None = None,
-) -> None:
-    """
-    Use case:
-    - забрать пачку RawNews, у которых есть embedding, но ещё нет события
-    - для каждой найти похожие новости по embedding в окне по времени
-    - решить: присоединить к существующему событию или создать новое.
-    """
-    if config is None:
-        config = ClusteringConfig()
-    # --------------------------------------------------------------------------------
-    # --- 1. забрать пачку RawNews по фильтру (у которых есть embedding, но ещё нет события)
-    # --------------------------------------------------------------------------------
-    items: list[RawNews] = await raw_repo.list_pending_for_event_clustering(limit=limit)
-
-    # создаем события
-    updated_raw_news = []  # связываем raw_news и event
-    new_events = []
-    updated_events = []
-
-    # --------------------------------------------------------------------------------
-    # --- 3. Обработка каждой сырой новости
-    # --------------------------------------------------------------------------------
-    for raw in items:
-
-        # Интуиция: Если time_window_before = 24 часа,
-        # то для новости в 15:00 мы смотрим события примерно с 15:00 вчера до 15:00 сегодня.
-        window_start = raw.published_at - config.time_window_before  # timedelta
-        window_end = raw.published_at + config.time_window_after  # timedelta
-
-        # 3.2. Ищем кандидаты-события - получить небольшой список событий,
-        # с которыми можно сравнить текущую новость
-
-
-        # смотрим ближайшие и косинусное расстояние отсеиваем по threshold также смотрим прочие фильтры
-        event_candidates: list[NewsEvent] = events_repo.get_news_events_candidates(raw, config) # совпадение по диапазону времени window_start, window_end и прочее по конфигу
-
-        if event_candidates:
-            similarity_candidates: dict[int, int] = await similarity_matcher.get_similar_news_candidates(raw, config)
-        else:
-            similarity_candidates: dict[int, int] = dict()
-
-        # Создание нового события (нет подходящих events)
-        if len(similarity_candidates.keys()) == 0:
-            event = await events_repo.create_from_raw_news(raw)  # новое событие
-            new_events.append(event)
-            # У raw появляется ссылка на это событие (на уровне доменной логики) — «я теперь в этом событии». event_id
-            # Это событие и обновлённая новость откладываются для последующего сохранения
-        # Добавление события к
-        else:
-            # {event_id: similarity_with_raw_news} -> выбираем самое близкое событие или еще просим llm сопоставить с пачкой
-            # принимаем решение создать Event или прикрепить новость к старом Event
-            #
-
-            # пропускаем кандидаты через ллм и проверяем (небольшую) пачку подходит это или нет
-            # я бы всё же это просто сделал на стороне агентов # todo: del this comment
-            chosen_event = await similarity_matcher.similaritychoose_event_for_raw_news(raw, similarity_candidates)
-            if chosen_event is None:
-                event = await events_repo.create_from_raw_news(raw)
-                new_events.append(event)
-            else:
-                event = await events_repo.attach_raw_to_existing_event(raw, chosen_event)
-                updated_events.append(event)
-
-        # обновляем только raw приписывая event_id в качестве fk
-        raw = raw.with_event_key(event.id)
-        updated_raw_news.append(raw)
-
-    # --------------------------------------------------------------------------------
-    # --- 4. Сохранение результатов
-    # --------------------------------------------------------------------------------
-    await raw_repo.update_many(updated_raw_news)  # обновляем raw_repo
-    await events_repo.update_many(updated_events)
-    await events_repo.save_many(new_events)
+        await self.events_repo.save_many(self.new_events)
