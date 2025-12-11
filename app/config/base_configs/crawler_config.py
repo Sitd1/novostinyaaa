5ea7f83a2e669f9a098d4fc85fccac17914b5835
denis.sitdikov <denis.sitd@yandex.ru>
2025-12-11 13:06:41 +0300

    ref crawler_config.py

--- app/config/base_configs/crawler_config.py
+++ app/config/base_configs/crawler_config.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 
 class TelegramConfig(ConfigBase):
-    model_config = SettingsConfigDict(env_prefix="TG_")  # FixMe: переделать как нужно в .env
+    model_config = SettingsConfigDict(env_prefix="CRAWLER_TG_")  # FixMe: переделать как нужно в .env
 
     api_id: SecretStr
     api_hash: SecretStr
