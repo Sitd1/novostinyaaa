9e262fc499684e0b645caed27bfaa1069ea9f3d6
denis.sitdikov <denis.sitd@yandex.ru>
2025-12-12 13:40:18 +0300

    ref make sync seccion

--- app/infrastructure/database/sync_connection/session.py
+++ app/infrastructure/database/sync_connection/session.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Session, sessionmaker
 from contextlib import contextmanager
 from typing import Generator
 
-from database.sync_connection.engine import engine as sync_engine
+from app.infrastructure.database.sync_connection.engine import engine as sync_engine
 
 # Фабрика сессий
 async_session_maker = sessionmaker(
@@ -16,13 +16,13 @@ async_session_maker = sessionmaker(
 
 # Контекстный менеджер для работы с сессией
 @contextmanager
-def get_session() -> Generator[Session, None]:
+def get_session() -> Generator[Session, None, None]:
     """
-    Асинхронный контекстный менеджер для получения сессии БД.
+    Синхронный контекстный менеджер для получения сессии БД.
 
     Использование:
-        async with get_session() as session:
-            result = await session.execute(query)
+        with get_session() as session:
+            result = session.execute(query)
     """
     with async_session_maker() as session:
         try:
