96955df780fd130e03fbd3fbf553d8a2625a2af1
denis.sitdikov <denis.sitd@yandex.ru>
2026-01-07 20:33:56 +0300

    add main

--- apps/crawler/main.py
+++ apps/crawler/main.py
@@ -5,3 +5,7 @@ from apps.crawler.services.tg_crawler.probe_collect import collect_last_messages
 
 def main():
     asyncio.run(collect_last_messages(limit_per_channel=50))
+
+
+if __name__ == "__main__":
+    main()
